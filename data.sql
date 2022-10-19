320c14c4c40cd0833e1c102d33e23d8cb6dc7d40
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-19 19:04:30 +0300

    insert and update data

--- data.sql
+++ data.sql
@@ -26,3 +26,36 @@ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg, s
     VALUES ('Blossom', '1998-10-13', 3, true, 17, 'Animal');
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg, species)
     VALUES ('Ditto', '2022-05-14', 4, true, 22, 'Animal');
+
+    INSERT INTO owners(full_name, age)
+    VALUES ('Sam Smith', 34);
+INSERT INTO owners(full_name, age)
+    VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners(full_name, age)
+    VALUES ('Bob', 45);
+INSERT INTO owners(full_name, age)
+    VALUES ('Melody Pond', 77);
+INSERT INTO owners(full_name, age)
+    VALUES ('Dean Winchester', 14);
+INSERT INTO owners(full_name, age)
+    VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species(name)
+    VALUES ('Pokemon');
+INSERT INTO species(name)
+    VALUES ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value: 
+UPDATE animals SET species_id=2 where name LIKE '%mon';
+UPDATE animals SET species_id=1 where name NOT LIKE '%mon';
+
+-- Modify your inserted animals to include owner information (owner_id): 
+UPDATE animals SET owners_id=1 WHERE name='Agumon';
+UPDATE animals SET owners_id=2 where name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id=3 where name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id=4 where name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id=5 where name IN ('Angemon', 'Boarmon');
+
+-- Update owners names with typo in Local DB
+UPDATE owners SET full_name='Dean Winchester' WHERE full_name like 'Dean Wincheste';
+UPDATE owners SET full_name='Jodie Whittaker' WHERE full_name like 'Jodie Whittake';
