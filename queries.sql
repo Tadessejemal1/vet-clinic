cbb4acae5b6b5461292504ecfa66d6600c702b58
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-19 19:15:01 +0300

    insert and update data

--- queries.sql
+++ queries.sql
@@ -16,6 +16,7 @@ SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT * FROM animals WHERE Weight_kg BETWEEN 10.4 AND 17.3;
 
 -- Inside a transaction update the animals table by setting the species column to unspecified.
+
 -- Verify that change was made. Then roll back the change and verify that species columns went back to the state before transaction.
 BEGIN;
 
