9bfe34fb6d79a735b397c81bc2c4cd254df50d03
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-26 22:18:16 +0300

    updated schema based on reviewor feedback

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,48 +1,63 @@
-CREATE TABLE patients (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR,
-    date_of_birth DATE
+/* Creating tables for clinic database and primery keys*/
+
+create table patients(
+id int generated always as identity,
+name varchar(60),
+date_of_birth date,
+primary key(id)
+)
+
+create table medical_histories(
+id int generated always as identity,
+admitted_at timestamp,
+patient_id int,
+status varchar(60),
+primary key(id)
 );
 
-CREATE TABLE invoices (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    total_amount DECIMAL,
-    generated_at TIMESTAMP,
-    payed_at TIMESTAMP,
-    medical_history_id INTEGER REFERENCES medical_histories(id)
+create table invoices(
+id int generated always as identity,
+total_amount decimal,
+generated_at timestamp,
+payed_at timestamp,
+medical_history_id  int,
+primary key(id)
 );
 
-CREATE TABLE medical_histories (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    admitted_at TIMESTAMP,
-    patient_id INTEGER REFERENCES patients(id),
-    status VARCHAR
+create table invoices_items(
+id int generated always as identity,
+unit_price decimal,
+quantity  int,
+total_price  decimal,
+invoice_id  int,
+treatment_id int,
+primary key(id)
 );
 
-CREATE TABLE invoice_items (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    unit_price DECIMAL,
-    quantity INTEGER,
-    total_price DECIMAL,
-    invoice_id INTEGER REFERENCES invoices(id),
-    treatment_id INTEGER REFERENCES treatments(id)
+create table treatments(
+id int generated always as identity,
+type varchar(60),
+name varchar(60),
+primary key(id)
 );
 
-CREATE TABLE treatments (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    type VARCHAR,
-    name VARCHAR
-);
+/*Creating  foreign keys for the relationship between one to many tables */
 
-CREATE TABLE medical_histories_treatments (
-    medical_history_id INTEGER REFERENCES medical_histories(id);
-    treatment_id INTEGER REFERENCES treatments(id)
-);
+ALTER TABLE patients ADD FOREIGN KEY (id) REFERENCES medical_histories(id);
+ALTER TABLE invoices ADD FOREIGN KEY (id) REFERENCES invoices_items(id);
 
-CREATE INDEX medical_history_index ON medical_histories(medical_history_id);
-CREATE INDEX treatment_index ON treatments(treatment_id);
-CREATE INDEX patients_index ON patients(patient_id);
-CREATE INDEX invoice_index ON invoices(invoice_id); 
-CREATE INDEX medical_history_index ON medical_histories_treatments(medical_history_id);
-CREATE INDEX treatment_index ON medical_histories_treatments(treatment_id);
+/* transition table between medical_histories and treatments */
 
+create table medical_form();
+
+/*Creating  foreign keys for the relationship between many to many tables */
+
+ALTER TABLE medical_form ADD COLUMN medical_histories_id INT REFERENCES medical_histories(id);
+ALTER TABLE medical_form ADD COLUMN treatments_id INT REFERENCES treatments(id);
+
+/* FK indexes */
+
+CREATE INDEX patients_refid ON patients(id);
+CREATE INDEX invoices_refid ON invoices(id);
+CREATE INDEX treatment_refid ON treatments(id);
+CREATE INDEX medical_histories_refid ON medical_histories(id);
