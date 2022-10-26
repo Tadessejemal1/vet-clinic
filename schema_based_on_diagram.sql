1ea589c231e3a7a6879172ec32579a40788c61d9
Tadesse Jemal <tadessejemal63@gmail.com>
2022-10-26 22:21:37 +0300

    updated the schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,44 +1,44 @@
 /* Creating tables for clinic database and primery keys*/
 
 create table patients(
-id int generated always as identity,
-name varchar(60),
-date_of_birth date,
-primary key(id)
+    id int generated always as identity,
+    name varchar(60),
+    date_of_birth date,
+    primary key(id)
 )
 
 create table medical_histories(
-id int generated always as identity,
-admitted_at timestamp,
-patient_id int,
-status varchar(60),
-primary key(id)
+    id int generated always as identity,
+    admitted_at timestamp,
+    patient_id int,
+    status varchar(60),
+    primary key(id)
 );
 
 create table invoices(
-id int generated always as identity,
-total_amount decimal,
-generated_at timestamp,
-payed_at timestamp,
-medical_history_id  int,
-primary key(id)
+    id int generated always as identity,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id  int,
+    primary key(id)
 );
 
 create table invoices_items(
-id int generated always as identity,
-unit_price decimal,
-quantity  int,
-total_price  decimal,
-invoice_id  int,
-treatment_id int,
-primary key(id)
+    id int generated always as identity,
+    unit_price decimal,
+    quantity  int,
+    total_price  decimal,
+    invoice_id  int,
+    treatment_id int,
+    primary key(id)
 );
 
 create table treatments(
-id int generated always as identity,
-type varchar(60),
-name varchar(60),
-primary key(id)
+    id int generated always as identity,
+    type varchar(60),
+    name varchar(60),
+    primary key(id)
 );
 
 /*Creating  foreign keys for the relationship between one to many tables */
